d4cd2bef922399d669b4fe0df76549ce700a4f81
luizrodrigues <luizh4321@gmail.com>
2024-04-11 21:27:10 -0300

    add fix carousel, display/ counter none

--- js/task.js
+++ js/task.js
@@ -1,10 +1,10 @@
-
+/*
 let count = 1;
 document.getElementById("radio1").checked = true;
   
 setInterval( function(){
     nextImage();
-}, 15000)
+}, 3000)
 
 function nextImage(){
     count++;
@@ -14,3 +14,26 @@ function nextImage(){
 
     document.getElementById("radio"+count).checked = true;
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    let currentIndex = 1;
+    const slides = document.querySelectorAll('.carousel-item');
+    const totalSlides = slides.length;
+
+    function showSlide(index) {
+      slides.forEach((slide, i) => {
+        if (i === index) {
+          slide.classList.add('active');
+        } else {
+          slide.classList.remove('active');
+        }
+      });
+    }
+
+    function nextSlide() {
+      currentIndex = (currentIndex + 1) % totalSlides;
+      showSlide(currentIndex);
+    }
+
+    setInterval(nextSlide, 5000);
+  });*/
